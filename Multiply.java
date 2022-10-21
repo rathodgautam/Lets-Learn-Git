9065fb5388be574fecf393ce303c9164bc98f93c
Gautam Rathod <rathodgautam7717@gmail.com>
2022-10-21 13:07:53 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package LetsLearnGit;
+
+public class Multiply {
+   public static void main(String[] args) {
+     
+    int a , b ,c ;
+    a  = 4 ;
+    b = 2;
+    System.out.println("The Product is :"+ (a * b));
+   }
+    
+} 
