f6fb458538ea28b1089b14e198d556e4006ca2a0
Gautam Rathod <rathodgautam7717@gmail.com>
2022-10-21 12:27:39 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package LetsLearnGit;
+ 
+public class Sum {
+    public static void main(String[] args) {
+       
+        int a ,b ,c;
+        a = 30 ;
+        b = 40 ;
+        c = a + b;
+        System.out.println(c);
+
+    }
+    
+}
